d13c7512994c3173e8251b6b19e33434281c854b
kyson <kyson@kysondeMacBook-Air.local>
2018-03-24 11:29:53 +0800

    [REV]appdelegate

--- Podfile
+++ Podfile
@@ -14,11 +14,4 @@ target "Wallpaper" do
         pod 'ReactiveObjC','3.1.0'
         pod 'JSDecoupledAppDelegate', '~> 1.1.0'
         pod 'BlocksKit'
-#        pod 'React', :path => './node_modules/react-native', :subspecs => [
-#        'Core',
-#        'RCTText’,
-#        'RCTNetwork',
-#        'RCTWebSocket',
-#        ]
-#        pod "yoga", :path => './node_modules/react-native/ReactCommon/yoga'
 end
--- Wallpaper/AppDelegate.m
+++ Wallpaper/AppDelegate.m
@@ -406,21 +406,6 @@
     // 开启网络监控
     AFNetworkReachabilityManager *netReachabilityManger = [AFNetworkReachabilityManager sharedManager];
     [netReachabilityManger startMonitoring];
-
-    
-//    NSString *urlString = @"http://192.168.0.:8081/index.ios.bundle?platform=ios";
-//    NSURL *jsCodeLocation = [NSURL URLWithString:urlString];
-//    RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-//                                                        moduleName:@"Wallpaper"
-//                                                 initialProperties:nil
-//                                                     launchOptions:launchOptions];
-//    rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-//
-//    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-//    UIViewController *rootViewController = [UIViewController new];
-//    rootViewController.view = rootView;
-//    self.window.rootViewController = rootViewController;
-//    [self.window makeKeyAndVisible];
     
     return YES;
 }
