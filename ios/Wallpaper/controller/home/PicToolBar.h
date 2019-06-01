a58fb6de4d5f7121be104d9702393adba6e58129
chy <chy66588@hotmail.com>
2019-06-01 13:34:17 +0800

    [FIX]修复了底部栏在全面屏机型下和安全区域重合的错误

---
+++ ios/Wallpaper/controller/home/PicToolBar.h
@@ -0,0 +1,17 @@
+//
+//  PicToolBar.h
+//  Wallpaper
+//
+//  Created by 陈浩宇 on 2019/5/31.
+//  Copyright © 2019 zhujinhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PicToolBar : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/Wallpaper/controller/home/PicToolBar.m
@@ -0,0 +1,21 @@
+//
+//  PicToolBar.m
+//  Wallpaper
+//
+//  Created by 陈浩宇 on 2019/5/31.
+//  Copyright © 2019 zhujinhui. All rights reserved.
+//
+
+#import "PicToolBar.h"
+
+@implementation PicToolBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
